dbad5754eba44ccbd39087726857dc7d56a0ab49
David Corvoysier <david@huggingface.co>
2025-03-20 10:18:24 +0000

    refactor(llama): remove unused ResBlock

--- src/neuronx_distributed_inference/models/llama/modeling_llama.py
+++ src/neuronx_distributed_inference/models/llama/modeling_llama.py
@@ -722,38 +722,6 @@ class NeuronLlamaDecoderLayer(nn.Module):
         return outputs
 
 
-class ResBlock(nn.Module):
-    """
-    A Residual Block module.
-
-    This module performs a linear transformation followed by a SiLU activation,
-    and then adds the result to the original input, creating a residual connection.
-
-    Args:
-        hidden_size (int): The size of the hidden layers in the block.
-    """
-
-    def __init__(self, hidden_size):
-        super().__init__()
-        self.linear = nn.Linear(hidden_size, hidden_size)
-        # Initialize as an identity mapping
-        torch.nn.init.zeros_(self.linear.weight)
-        # Use SiLU activation to keep consistent with the Llama model
-        self.act = nn.SiLU()
-
-    def forward(self, x):
-        """
-        Forward pass of the ResBlock.
-
-        Args:
-            x (torch.Tensor): Input tensor.
-
-        Returns:
-            torch.Tensor: Output after the residual connection and activation.
-        """
-        return x + self.act(self.linear(x))
-
-
 class NeuronLlamaModel(NeuronDecoderModel):
     """
     The neuron version of the LlamaModel
